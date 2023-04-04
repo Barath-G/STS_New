25a6a819573a93e49c5c8e2115f7a25ba8dfbe45
Barath-G <bharathjovialg@gmail.com>
2023-04-04 11:22:47 +0530

    H

---
+++ Firstproject/src/Practice01/two.java
@@ -0,0 +1,7 @@
+package Practice01;
+
+public class two {
+	public static void main (String[] args) {
+		
+	}
+}
---
+++ Firstproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rajap
+ *
+ */
+module Firstproject {
+}
