a3b345fe416ddfe6519afb7a9f38aad21b4610e3
Barath-G <bharathjovialg@gmail.com>
2023-04-04 11:20:40 +0530

    H

--- Project-1/src/main/java/com/example/demo/Home.java
+++ Project-1/src/main/java/com/example/demo/Home.java
@@ -1,8 +1,42 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
-public class Home {
+@Component
+@Scope(value="Prototype")
+public class Home 
+{
+	public Home()
+	{
+		System.out.println("Home Home");
+	}
+
+	private String owner;
+	private int doorNo;
+	
+	@Autowired
+	@Qualifier("abc")
+	private InternetConnection modem;
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public int getDoorNo() {
+		return doorNo;
+	}
+	public void setDoorNo(int doorNo) {
+		this.doorNo = doorNo;
+	}
+	public void connect() {
+		System.out.println("Connecting to Internet");
+	}
+	
 	
 	
 }
--- Project-1/src/main/java/com/example/demo/Project1Application.java
+++ Project-1/src/main/java/com/example/demo/Project1Application.java
@@ -2,14 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Project1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project1Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Project1Application.class, args);
 	
-		
+	 			
+			Home h = context.getBean(Home.class);
+			h.connect();
+			
+			Home h2 = context.getBean(Home.class);
+			h2.connect();
 	}
 
 }
